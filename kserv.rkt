902afab6969f172313a1047bb373a0ea426e7077
Dmitry Cherkassov <dcherkassov@gmail.com>
2014-06-22 02:20:53 +0400

    add error checking and number input box

--- kserv.rkt
+++ kserv.rkt
@@ -1,5 +1,5 @@
 #lang racket
-; version 0.12
+; version 0.13
 
 (require web-server/servlet
          web-server/servlet-env)
@@ -14,8 +14,13 @@
          [kmd (if (exists-binding? 'kmd bs)
                   (string->number (extract-binding/single 'kmd bs))
                    10)]
-         [ktext (if (eq? kinput #f) ""
-                    (kokoify-text kinput kmd))])
+         [ktext (if (eq? kmd #f)
+                    (begin
+                      (set! kmd 10)
+                      (format "Error: Ты, это давай КОКОКО отсюда с такими числами: ~a"  (extract-binding/single 'kmd bs))
+                      )
+                    (if (eq? kinput #f) ""
+                        (kokoify-text kinput kmd)))])
     (response/xexpr
      `(html (head (title "KaS (Kurkuma-as-service) (2017)"))
             (body (p (font ([size "4"]) "Веб-ебло куркуматора."))
@@ -23,7 +28,7 @@
                   (form ([action "/kurkumator.rkt"])
                         (p "Копируй сюда свою пасту: ")
                         (TEXTAREA ([rows "4"] [cols "50"] [name "ktext"]) ,kinput )
-                        (p "Zashquar magntitude: " (input ([name "kmd"] [value ,(number->string kmd)])))
+                        (p "Zashquar magntitude: " (input ([type "number"] [min "1"] [max "20"] [name "kmd"] [value ,(number->string kmd)])))
                         (p (input ([type "submit"] [value "Mamka ipal)))"])))
                         (p (font ([size "1"]) "Версия 0.12пук"))))))))
  
@@ -32,5 +37,6 @@
                #:listen-ip #f
                #:servlet-path "/kurkumator.rkt"
                #:log-file "kurkumator.log"
-               #:command-line? #t)
+               #:command-line? #t
+               )
 
